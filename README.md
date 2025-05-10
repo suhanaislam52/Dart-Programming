a7385fd3fd2304ec446399a49d563cb3b3fa75d1
Suhana Islam <suhanaislam52@gmail.com>
2025-05-10 20:48:36 +0600

    Initial commit of Dart Programming project

---
+++ README.md
@@ -0,0 +1,2 @@
+# dart-basic-programming
+Basic Dart Programming
---
+++ dartbasic.dart
@@ -0,0 +1,19 @@
+void main(){
+    print("Hello world");
+
+    String Name="Dhruv";
+    Name="Arne";
+    int pincode=454001;
+    print(Name);
+    pincode=12345;
+    print(pincode);
+    bool isRaining=false;
+    print(isRaining);
+
+
+    dynamic Address="Delhi";
+    Address=1234;
+    print(Address);
+      
+
+}
